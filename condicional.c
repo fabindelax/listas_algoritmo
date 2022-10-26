5c4b0b07b551e8d0dc05c4509f1392ceea5eb044
fabindelax <113560771+fabindelax@users.noreply.github.com>
2022-10-26 20:30:51 -0300

    Add files via upload

---
+++ condicional.c
@@ -0,0 +1,24 @@
+//escreva um algoritmo que receba uma idade e informe se o usuario podera ou não votar
+
+
+#include<stdio.h>
+int main(){
+    int idade;
+    
+        printf("informe a idade desejada");
+        scanf("%d", &idade);
+
+        // se (idade >= 16)
+        // entao: escreva ("Pode votar!");
+        // senao escreva ("Não pode votar!")
+
+        
+        if (idade >= 16)
+        printf("Pode votar!\n"); // codigo que será executado caso a condição seja Verdadeira
+        else printf("Não pode votar!\n"); // codigo que será executado caso a condição seja Falsa 
+
+
+
+
+}
+
